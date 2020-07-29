81361936924bd37cdf3589b29fecd601cd39322e
jingwenyi <1124427436@qq.com>
2020-07-29 07:25:41 -0700

    add algorithm code for image_project4.cpp

--- Image_project/image_project4.cpp
+++ Image_project/image_project4.cpp
@@ -10,6 +10,14 @@ using namespace std;
 using namespace cv;
 
 
+#define  IMAGE1_NUMBER_OF_SAMPLES   4000
+#define  NUMBER_OF_INTERVAL_ROWS    100
+#define  IMAGE2_NUMBER_OF_SAMPLES   4100
+
+
+
+
+
 //AB 航线上有27 张图片，对应27 个偏航角
 const static float yaw_AB[] ={	92.35f,	91.36f, 87.14f, 91.96f, 85.27f, 94.03f, 90.73f, 95.61f, 95.35f,
 							  	94.07f, 99.07f, 88.51f, 89.47f, 90.03f, 90.48f, 87.24f, 88.03f, 91.4f,
@@ -45,19 +53,6 @@ int main(int argc, char *argv[])
 
 	//AB:91.7618, CD:284.542
 	cout << "yaw AB avg:" << yaw_AB_avg << ",yaw CD avg:" << yaw_CD_avg << endl;
-#if 0
-	//申请一张很大的画布，画布的大小可以由航线上照片的数量估计出来，这里是任意的估计的
-	Mat mymap( 10000, 10000,CV_8UC3);
-
-	//循环读取每一张图片DSC00622.JPG~DSC00677.JPG
-	for(int i=0; i<56; i++)
-	{
-		
-	}
-	
-	mymap.setTo(100);
-	imwrite("mymap.jpg", mymap);
-#endif
 
 	
 	Mat image01 = imread("/home/wenyi/workspace/test_photo/DSC00622.JPG");
@@ -92,7 +87,89 @@ int main(int argc, char *argv[])
 int Overlapping_image_mosaic_algorithm( Mat &image1, Mat &image2, int &x_dis, int &y_dis)
 
 {
+	//快速匹配，由于匹配效果很好，没有添加连续性检查代码
+	//在image1 中的上半部分 1/2 出，取一个4000*100 的匹配块
+	int base[IMAGE1_NUMBER_OF_SAMPLES];
+	int start_row = (image1.rows / 4);
+	int start_col = (image1.cols / 2) - IMAGE1_NUMBER_OF_SAMPLES/2;
+
+	for(int i=0; i<IMAGE1_NUMBER_OF_SAMPLES; i++)
+	{
+		base[i] = image1.at<uchar>(start_row - NUMBER_OF_INTERVAL_ROWS, start_col + i) - image1.at<uchar>(start_row, start_col + i);
+		image1.at<uchar>(start_row - NUMBER_OF_INTERVAL_ROWS, start_col + i) = 0;
+		image1.at<uchar>(start_row, start_col + i) = 0;
+	}
+
+	int num = image2.rows  - NUMBER_OF_INTERVAL_ROWS;
+	int match_start_col = image2.cols / 2 - IMAGE2_NUMBER_OF_SAMPLES / 2;
+
+	int min_all_err = INT_MAX;
+	int min_all_num = 0;
+	int min_all_num_dis = 0;
+
+	int min_err[num];
+	int min_err_dis[num] = {0};
+
+	for(int i=0; i<num; i++)
+	{
+		min_err[i] = INT_MAX;
+	}
+
+	int match_image[IMAGE2_NUMBER_OF_SAMPLES];
+	int dis = IMAGE2_NUMBER_OF_SAMPLES - IMAGE1_NUMBER_OF_SAMPLES + 1;
+
+	for(int n = 0; n < num; n++)
+	{
+		for(int i=0; i<IMAGE2_NUMBER_OF_SAMPLES; i++)
+		{
+			match_image[i] = image2.at<uchar>(n, match_start_col + i) -
+								 image2.at<uchar>(n + NUMBER_OF_INTERVAL_ROWS, match_start_col + i);
+		}
+
+		for(int i=0; i<dis; i++)
+		{
+			int err = 0;
+			for(int j=0; j<IMAGE1_NUMBER_OF_SAMPLES; j++)
+			{
+				err += pow((match_image[j+i] - base[j]), 2);
+			}
+
+			if(err < min_err[n])
+			{
+				min_err[n] = err;
+				min_err_dis[n] = i;
+
+				if(min_err[n] < min_all_err)
+				{
+					min_all_err = min_err[n];
+					min_all_num_dis = min_err_dis[n];
+					min_all_num = n;
+				}
+			}
+
+		}
+	}
+
+#if 1
+	for(int n = 0; n<num; n++)
+	{
+		cout << "num:" << n << ",min_err_dis:" << min_err_dis[n] << ",min_err:" << min_err[n] << endl;
+	}
 	
+	cout << "min all err:" << min_all_err << ",min all num:" << min_all_num << ",min all num dis:" << min_all_num_dis << endl;
+	
+	for(int i=0; i<IMAGE1_NUMBER_OF_SAMPLES; i++)
+	{
+		image2.at<uchar>(min_all_num, match_start_col + min_all_num_dis + i) = 0;
+		image2.at<uchar>(min_all_num+NUMBER_OF_INTERVAL_ROWS, match_start_col + min_all_num_dis + i) = 0;
+	}
+#endif
+
+	//y < 0, 表示向左 移动的像素，y > 0 表示向 右移动的像素
+	y_dis = (IMAGE2_NUMBER_OF_SAMPLES - IMAGE1_NUMBER_OF_SAMPLES) / 2 - min_all_num_dis;
+	//x > 0, 向上移动了14 个像素点
+	x_dis = min_all_num - start_row + NUMBER_OF_INTERVAL_ROWS;
+
 	return 0;
 }
 
