6af3aa18b1ec2fbac56061398db230c4999527ff
jingwenyi <1124427436@qq.com>
2020-09-04 10:18:51 -0700

    add a new project last

---
+++ last_project/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/xfeatures2d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <math.h>
+#include <limits.h>
+using namespace std;
+using namespace cv;
+
+
+
+int main()
+{
+	cout << "hello world" << endl;
+	return 0;
+}
