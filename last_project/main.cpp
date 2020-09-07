bef7d6b4c3af133160be54401195feb8525bda2e
jingwenyi <1124427436@qq.com>
2020-09-07 09:14:15 -0700

    add read gps and imu date.

--- last_project/main.cpp
+++ last_project/main.cpp
@@ -15,6 +15,8 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
+
+#if 0
 	//测试读取所有的图像名称image_name.txt
 	string strFile = "/home/wenyi/workspace/DCIM/10000904/image_name.txt";
 
@@ -32,23 +34,60 @@ int main(int argc, char **argv)
         if(!s.empty())
         {
         	cout << s << endl;;
-
-		#if 0
-            stringstream ss;
-            ss << s;
-            double t;
-            string sRGB;
-            ss >> t;
-            vTimestamps.push_back(t);
-            ss >> sRGB;
-            vstrImageFilenames.push_back(sRGB);
-		#endif
         }
 		
 	}
-	
-	
-	
+#endif
+
+
+	//测试读取gps 和imu 数据
+	string strFile = "/home/wenyi/workspace/DCIM/10000904/image_gps_imu.txt";
+
+	ifstream f;
+    f.open(strFile.c_str());
+
+	// skip first one lines
+	string s0;
+	getline(f,s0);
+
+	cout << "lat\t" << "lng\t" << "alt\t" << "roll\t" << "pitch\t" << "yaw" << endl;
+	 while(!f.eof())
+    {
+		string s;
+        getline(f,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double lat;
+			ss >> lat;
+
+			double lng;
+			ss >> lng;
+
+			double alt;
+			ss >> alt;
+
+			double alt_rel;
+			ss >> alt_rel;
+
+			double alt_gps;
+			ss >> alt_gps;
+
+			double roll;
+			ss >> roll;
+
+			double pitch;
+			ss >> pitch;
+
+			double yaw;
+			ss >> yaw;
+		
+		cout << setprecision(15) << lat << "\t" <<lng << "\t" << alt << "\t" << roll << "\t" << pitch << "\t" << yaw << endl;
+
+        }
+		
+	}
 
 	cout << "I am ok" << endl;
 	return 0;
