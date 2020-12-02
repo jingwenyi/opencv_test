3bf3ebda1389eb3c5ba004a7baa9d46ef68ecea0
jingwenyi <1124427436@qq.com>
2020-12-01 22:16:24 -0800

    start write demo.

---
+++ demo/Image_Mosaic_Demo.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+struct Location {
+	Location(int _alt=0, int _lat=0, int _lng=0):
+		alt(_alt), lat(_lat), lng(_lng)
+	{
+	}
+	
+    int alt:24;                                     ///Altitude in centimeters (meters * 100) see LOCATION_ALT_MAX_M
+    int lat;                                        /// Latitude * 10**7
+    int lng;                                        ///Longitude * 10**7
+};
+
+struct Imu_data {
+	Imu_data(float _pitch=0, float _roll=0, float _yaw=0):
+		pitch(_pitch), roll(_roll), yaw(_yaw)
+	{
+	}
+
+	float pitch;
+	float roll;
+	float yaw;
+};
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+	vector<string>  image_name;
+	vector<Location> gps_data;
+	vector<Imu_data> imu_data;
+
+
+	string strFile = "../plane_image/image_name.txt";
+
+	ifstream f;
+    f.open(strFile.c_str());
+
+	// skip first one lines
+    string s0;
+    getline(f,s0);
+
+	 while(!f.eof())
+    {
+		string s;
+        getline(f,s);
+        if(!s.empty())
+        {
+        	image_name.push_back(s);
+        }
+		
+	}
+
+	f.close();
+	strFile.clear();
+
+#if 0
+	for(auto name:image_name)
+	{
+		cout << name << endl;
+	}
+#endif
+
+	strFile = "../plane_image/gps.txt";
+
+	f.open(strFile.c_str());
+	// skip first one lines
+    getline(f,s0);
+
+	while(!f.eof())
+    {
+		string s;
+        getline(f,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double lat;
+			ss >> lat;
+
+			double lng;
+			ss >> lng;
+
+			double alt;
+			ss >> alt;
+
+			struct Location gps(alt * 100, lat * 1.0e7, lng * 1.0e7);
+			gps_data.push_back(gps);
+        }
+	}
+	
+	f.close();
+	strFile.clear();
+#if 0
+	for(auto gps:gps_data)
+	{
+		cout << gps.alt << "\t" << gps.lat << "\t" << gps.lng << endl;
+	}
+#endif
+
+	strFile = "../plane_image/imu.txt";
+
+	f.open(strFile.c_str());
+	// skip first one lines
+    getline(f,s0);
+
+	while(!f.eof())
+    {
+		string s;
+        getline(f,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+
+			double roll;
+			ss >> roll;
+
+			double pitch;
+			ss >> pitch;
+
+			double yaw;
+			ss >> yaw;
+
+			struct Imu_data imu(pitch, roll, yaw);
+			imu_data.push_back(imu);
+        }
+	}
+	
+	f.close();
+	strFile.clear();
+
+#if 0
+	for(auto imu:imu_data)
+	{
+		cout << imu.pitch << "\t" << imu.roll << "\t" <<imu.yaw << endl;
+	}
+#endif
+
+	
+
+
+	return 0;
+}
+
